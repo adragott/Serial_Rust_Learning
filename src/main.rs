6e584991b99e9b4b1fb17df48504ae9f7c1a5915
Penguin <ad0097@uah.edu>
2020-01-09 20:03:23 -0600

    Got something working!

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ extern crate serial;
 use std::env;
 use std::io;
 use std::time::Duration;
-
+use std::str;
 use std::io::prelude::*;
 use serial::prelude::*;
 
@@ -28,10 +28,53 @@ fn interact<T: SerialPort>(port: &mut T) -> io::Result<()> {
     loop
     {
         let mut buf: Vec<u8> = (0..255).collect();
+        let retthing = match port.read(&mut buf[..])
+        {
+            Ok(v) => v,
+            Err(e) => 
+            {
+                println!("{}", e);
+                continue;
+            },
+        };
+        
+        let mut out = String::new();
+        loop
+        {
+            match str::from_utf8(&buf)
+            {
+                Ok(s) => 
+                {
+                    out.push_str(s);
+                    break;
+                }
+                Err(e) => 
+                {
+                    let (good, bad) = buf.split_at(e.valid_up_to());
+                    if !good.is_empty()
+                    {
+                        let s = unsafe 
+                        {
+                            // This is safe because we have already validated this
+                            // UTF-8 data via the call to str::from_utf8`; There's
+                            // no need to check it a second time
+                            str::from_utf8_unchecked(good)
+                        };
+                        out.push_str(s);
+                    }
+                    
+                    if bad.is_empty()
+                    {
+                        break;
+                    }
 
-        port.read(&mut buf[..]);
-        let s = String::from_utf8_lossy(&buf);
-        println!("{}", s);
+                    buf = bad[1..].to_vec();
+                
+                }
+            }
+        }
+        
+        println!("{}", out);
     }
     Ok(())
 }
